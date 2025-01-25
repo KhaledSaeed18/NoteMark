6365563a88fe3d4398bf7f5d271b0a013943dee2
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-01-25 21:42:28 +0200

    Add Markdown editor component and integrate with Tailwind CSS

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,4 @@
-import { ActionButton, ActionButtonsRow, Content, NotePreviewList, RootLayout, Sidebar } from "@/components"
+import { ActionButton, ActionButtonsRow, Content, MarkdownEditor, NotePreviewList, RootLayout, Sidebar } from "@/components"
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
         <NotePreviewList className="mt-3 space-y-1" />
       </Sidebar>
       <Content className="border-l bg-zinc-900/50 border-l-white/20">
-        Content
+        <MarkdownEditor />
       </Content>
     </RootLayout>
   )
