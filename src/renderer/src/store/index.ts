3537d66fed8a1e5f604b9a62f2fd4f54cdb657c6
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-01-25 22:01:33 +0200

    Add jotai for state management and implement notes list hook

---
+++ src/renderer/src/store/index.ts
@@ -0,0 +1,21 @@
+import { atom } from "jotai";
+import { notesMock } from "./mocks";
+import { NoteInfo } from "@shared/models";
+
+export const notesAtom = atom<NoteInfo[]>(notesMock)
+
+export const selectedNoteIndexAtom = atom<number | null>(null)
+
+export const selectedNoteAtom = atom((get) => {
+    const selectedNoteIndex = get(selectedNoteIndexAtom)
+    const notes = get(notesAtom)
+
+    if (!selectedNoteIndex) return null
+
+    const selectedNote = notes[selectedNoteIndex]
+
+    return {
+        ...selectedNote,
+        content: `# Hello from ${selectedNoteIndex}`
+    }
+})
