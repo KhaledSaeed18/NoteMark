2bd385c1366a1926b12307baebcb0ab4c6f479d3
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-01-26 19:17:49 +0200

    Implement note creation and deletion functionality with Jotai state management

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -19,3 +19,36 @@ export const selectedNoteAtom = atom((get) => {
         content: `# Hello from ${selectedNoteIndex}`
     }
 })
+
+export const createEmptyNoteAtom = atom(null, async (get, set) => {
+    const notes = get(notesAtom)
+
+    if (!notes) return
+
+    const title = `Note ${notes.length + 1}`
+
+    if (!title) return
+
+    const newNote: NoteInfo = {
+        title,
+        lastEditTime: Date.now()
+    }
+
+    set(notesAtom, [newNote, ...notes.filter((note) => note.title !== newNote.title)])
+
+    set(selectedNoteIndexAtom, 0)
+})
+
+export const deleteNoteAtom = atom(null, async (get, set) => {
+    const notes = get(notesAtom)
+    const selectedNote = get(selectedNoteAtom)
+
+    if (!selectedNote || !notes) return
+
+    set(
+        notesAtom,
+        notes.filter((note) => note.title !== selectedNote.title)
+    )
+
+    set(selectedNoteIndexAtom, null)
+})
